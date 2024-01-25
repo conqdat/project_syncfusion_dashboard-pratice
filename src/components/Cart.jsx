fb7b6b4fb82d84e196d190c113bbf926158e94b4
DatTran <trancongdat.datct@gmail.com>
2024-01-25 17:24:16 +0700

    init project

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className="bg-half-transparent w-full fixed nav-item top-0 right-0 ">
+      cart
+    </div>
+  )
+}
+
+export default Cart
